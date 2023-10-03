f8b2b1801aac7560d90a25333af2c8cace412553
Cleyson <cleysonph@gmail.com>
2023-10-03 01:24:55 -0300

    fix: corrige teste que não estava passando

--- math_test.go
+++ math_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(5, 5)
+	total := soma(5, 5)
 	if total != 10 {
-		t.Errorf("Soma(5, 5) = %d; want 10", total)
+		t.Errorf("soma(5, 5) = %d; want 10", total)
 	}
 }
